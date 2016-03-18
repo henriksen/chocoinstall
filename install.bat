ddb2a55d2095273b9e3a832a448d22defba4e376
Glenn F. Henriksen <glenn@henriksen.no>
2016-03-18 09:20:22 +0100

    Added command to install Chocolatey

--- install.bat
+++ install.bat
@@ -1,3 +1,5 @@
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
 choco install git.install 
 choco install visualstudiocode 
 choco install sourcecodepro 
